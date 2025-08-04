8aaddcd977a68ef3a90456f99681acbe308bc09a
alex <mo1or@mail.ru>
2025-08-04 19:40:25 +0400

    add addInArrayOnePlusEnd

--- test_examples/src/AddInArrayOnePlusInEnd.java
+++ test_examples/src/AddInArrayOnePlusInEnd.java
@@ -7,23 +7,22 @@ public class AddInArrayOnePlusInEnd {
     * */
 
     public static int[] returnArrayOnePlusInEnd(int[] arr) {
-        int next = 0;
         for (int i = arr.length-1; i>0; i--) {
-            if (arr[i] < 9) {
-                arr[i] = arr[i]+1;
-                return arr;
+            if (arr[i] == 9) {
+                arr[i] = 0;
             }
             else {
-                if (next > 0) arr[i] = next;
-                next = arr[--i] + 1;
-                arr[i] = 0;
+                if ( (arr[i] + 1) < 9) {
+                    arr[i] = arr[i] + 1;
+                    return arr;
+                }
             }
         }
         return arr;
     }
 
     public static void main(String[] args) {
-        int[] array = {1,2,3,9,9};
+        int[] array = {1,2,9,9,5};
         System.out.println(Arrays.toString(returnArrayOnePlusInEnd(array)));
     }
 }
